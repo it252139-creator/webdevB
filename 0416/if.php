4a66f6c71b7835c361fcdf2920f9a1d951dc7435
it252139-creator <it252139@trident.ac.jp>
2026-04-16 12:41:34 +0900

    0416

---
+++ 0416/if.php
@@ -0,0 +1,22 @@
+<?php
+$condition = false;
+if ($condition)
+    echo "条件はtrueです。";
+
+$a = 2;
+if ($a === 1) {
+    echo "aは1です。";
+}
+
+$b = 1;
+$c = 2;
+if ($b < $c) {
+    echo "bはcより小さいです。";
+}
+
+echo "<br>";
+
+$d = "1";
+if ($d == 1) {
+    echo "dは1です。";
+}
