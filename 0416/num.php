25b192ef6ccf4f25869e333b08ee34f340a4a132
it252139-creator <it252139@trident.ac.jp>
2026-04-16 11:44:39 +0900

    Initial commit

---
+++ 0416/num.php
@@ -0,0 +1,11 @@
+<?php
+$a = 10;
+$b = 2;
+$c = 3;
+
+echo $a + $b;
+echo $a - $b;
+echo $a * $b;
+echo $a / $b;
+echo $a % $c;
+echo $a ** 2;
---
+++ 0416/variable1.php
@@ -0,0 +1,7 @@
+<?php
+$greeting = "こんばんは";
+echo $greeting; //変数の中身を表示
+$price = "1000.236";
+echo $price;
+var_dump($greeting);
+var_dump($price);
