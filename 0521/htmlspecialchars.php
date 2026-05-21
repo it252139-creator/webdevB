84ba200a656ffbe8a0fb6a2fafe7019d50a2db98
it252139-creator <it252139@trident.ac.jp>
2026-05-21 12:41:02 +0900

    簡単なWebアプリ
    練習問題10に途中

---
+++ 0521/htmlspecialchars.php
@@ -0,0 +1,8 @@
+<?php
+//htmlspecialchars1.php
+echo "<s>test</s><br>" . PHP_EOL;
+echo htmlspecialchars("<s>test</s><br>", ENT_QUOTES, 'UTF-8');
+
+//htmlspecialchars2.php
+echo htmlspecialchars($_POST['a'], ENT_QUOTES, 'UTF-8');
+//echo $_POST['a'];
---
+++ 0521/question10/search.php
@@ -0,0 +1,4 @@
+<?php
+require_once './search.php';
+$fp = fopen("songs.csv", "r");
+var_dump($fp);
